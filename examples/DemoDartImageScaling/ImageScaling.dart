cb51a85115519be842cf2500c341786a0fb68f71
Brandon Donnelson <branflake2267@gmail.com>
2012-07-01 15:35:12 -0700

    Dart Demo Image Scaling

---
+++ examples/DemoDartImageScaling/ImageScaling.dart
@@ -0,0 +1,60 @@
+#import('dart:html');
+
+void main() {
+  new ImageScaler.start();
+}
+
+class ImageScaler {
+  
+  ImageScaler.start() {
+    observeFileInput();
+  }
+  
+  void observeFileInput() {
+    InputElement fileElement = query("#file");
+    fileElement.on.change.add((Event e) {
+      processFiles(fileElement.files);
+    }, true);
+  }
+  
+  void processFiles(List<File> files) {
+    for (File file in files) {
+      processFile(file);
+    }
+  }
+  
+  void processFile(File file) {
+    print("file.name=${file.name}");
+    
+    if (!isAnImage(file)) {
+      window.alert("Oops, that wasn't an image, can you try an image?");
+      return;
+    }
+    
+    readIn(file);
+  }
+  
+  bool isAnImage(File file) {
+    print("file.type=${file.type}");
+    
+    Pattern pattern = new RegExp(@"(jpeg|png)");
+    
+    bool b = false;
+    if (file.type.toString().contains(pattern) == true) {
+      b = true;
+    }
+    
+    return b;
+  }
+  
+  void readIn(File file) {
+    FileReader reader = new FileReader();
+    reader.on.loadEnd.add((e) => scale(reader.result));
+    reader.readAsDataURL(file);
+  }
+  
+  void scale(String base64) { // getting called twice???
+    print(base64);
+  }
+
+}
